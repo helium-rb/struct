688e5e3fa92473346cd509951714e6cdf94fc12f
Stanislaw Klajn <sklajn@gmail.com>
2021-06-12 09:16:36 +0100

    Empty gem

---
+++ lib/helium/struct.rb
@@ -0,0 +1,8 @@
+require "helium/struct/version"
+
+module Helium
+  module Struct
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/helium/struct/version.rb
@@ -0,0 +1,5 @@
+module Helium
+  module Struct
+    VERSION = "0.1.0"
+  end
+end
